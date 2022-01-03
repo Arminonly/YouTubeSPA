ec108e43cf158427f71518aa3eed04ef0cb3c735
Arminonly <amiaby2016@gmail.com>
2022-01-03 21:30:12 +0300

    Updates

---
+++ precache-manifest.195b4296731730d387ebdb380d3dd422.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b32978cd7915200096a5",
+    "url": "/YouTubeSPA/static/css/main.f92adc43.chunk.css"
+  },
+  {
+    "revision": "b32978cd7915200096a5",
+    "url": "/YouTubeSPA/static/js/main.b32978cd.chunk.js"
+  },
+  {
+    "revision": "d77314b6af56bd38dfbf",
+    "url": "/YouTubeSPA/static/js/1.d77314b6.chunk.js"
+  },
+  {
+    "revision": "c48ac4615e5a29b3ebe5",
+    "url": "/YouTubeSPA/static/css/2.743a8509.chunk.css"
+  },
+  {
+    "revision": "c48ac4615e5a29b3ebe5",
+    "url": "/YouTubeSPA/static/js/2.c48ac461.chunk.js"
+  },
+  {
+    "revision": "3f0eeb168ed325fbc329",
+    "url": "/YouTubeSPA/static/js/runtime~main.3f0eeb16.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/YouTubeSPA/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6846c290a5c063d5639918642be361d7",
+    "url": "/YouTubeSPA/index.html"
+  }
+];
